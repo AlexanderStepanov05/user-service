67e63e1fd0fa61e23d7e9ce7bcbb5e3cc0224240
AlexanderStepanov05 <alexanderstepanov2002@mail.ru>
2024-01-04 13:53:36 +0300

    modify UserServicE.JAvA

--- src/main/java/justebin/user/service/UserService.java
+++ src/main/java/justebin/user/service/UserService.java
@@ -1,4 +1,26 @@
 package justebin.user.service;
+import justebin.user.model.User;
+import justebin.user.repository.UserRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.stereotype.Service;
 
+@Service
 public class UserService {
+
+    //@Autowired
+    private UserRepository userRepo;
+
+    //@Autowired
+    private BCryptPasswordEncoder passwordEncoder;
+
+    public User registerNewUser(User user) {
+        user.setPassword(passwordEncoder.encode(user.getPassword()));
+        return userRepo.save(user);
+    }
+
+    public User getUserByUsername(String username) {
+        return userRepo.findByUsername(username);
+    }
+
 }
