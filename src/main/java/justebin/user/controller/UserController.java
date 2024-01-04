9b1ebb8f1748655293669022db8bdce3fe9ab2b6
AlexanderStepanov05 <alexanderstepanov2002@mail.ru>
2024-01-04 16:07:45 +0300

    modify UserController.java

--- src/main/java/justebin/user/controller/UserController.java
+++ src/main/java/justebin/user/controller/UserController.java
@@ -1,4 +1,28 @@
 package justebin.user.controller;
 
+import justebin.user.model.User;
+import justebin.user.service.UserService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+@RestController
+@RequestMapping("/api/users")
 public class UserController {
+
+    @Autowired
+    private UserService userService;
+
+    @PostMapping("/register")
+    public ResponseEntity<User> registerNewUser(@RequestBody User user) {
+        User registeredUser = userService.registerNewUser(user);
+        return new ResponseEntity<>(registeredUser, HttpStatus.CREATED);
+    }
+
+    @GetMapping("/{username}")
+    public ResponseEntity<User> getUserByUsername(@PathVariable String username) {
+        User user = userService.getUserByUsername(username);
+        return new ResponseEntity<>(user, HttpStatus.OK);
+    }
 }
